60bb8c6ca93ea5d11081bda63e90888182daa462
marta <martachmlima@gmail.com>
2022-01-17 11:57:30 -0300

    feat/ signIn and singOut ready

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -15,6 +15,7 @@ import { FaEnvelope, FaLock } from "react-icons/fa";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
+import { useAuth } from "../../contexts/AuthContext";
 
 const signInSchema = yup.object().shape({
   email: yup.string().required("Email obrigatório").email("Email inválido"),
@@ -29,6 +30,8 @@ interface SignInData {
 export const LogIn = () => {
   const [loading, setLoading] = useState(false);
 
+  const { signIn } = useAuth();
+
   const {
     formState: { errors },
     register,
@@ -37,40 +40,57 @@ export const LogIn = () => {
     resolver: yupResolver(signInSchema),
   });
 
-  const handleSignIn = (data: SignInData) => console.log(data);
+  const handleSignIn = (data: SignInData) => {
+    setLoading(true);
+    signIn(data)
+      .then((_) => setLoading(false))
+      .catch((err) => setLoading(false));
+  };
 
   return (
     <Flex
+      justifyContent="center"
       alignItems="center"
-      padding="10px 15px"
+      padding={["10px 15px", "10px 15px", "0px", "0px"]}
       color="white"
-      height="100vh"
-      bgGradient="linear(to-r, purple.800 65%, white 35%)"
+      height={["auto", "auto", "100vh"]}
+      bgGradient={[
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-b, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+        "linear(to-r, purple.800 65%, white 35%)",
+      ]}
     >
       <Flex
-        w="100%"
+        w={["100%", "100%", "90%", "65%"]}
         justifyContent="center"
-        flexDirection="row"
+        flexDirection={["column", "column", "row", "row"]}
         alignItems="center"
       >
-        <Grid w="100%" paddingLeft="100px">
-          <Image src={PrimLogo} alt="doit" boxSize="120px" />
-          <Heading as="h1">O jeito fácil, grátis</Heading>
-          <Text>
+        <Grid w={["100%", "100%", "50%", "40%"]} paddingLeft="0px">
+          <Image
+            src={PrimLogo}
+            alt="doit"
+            boxSize={["120px", "120px", "150px", "150px"]}
+          />
+          <Heading as="h1" mt="4">
+            O jeito fácil, grátis
+          </Heading>
+          <Text w={["150px", "250px", "350px"]}>
             Flexível e atrativo de gerenciar
-            <b>seus projetos em uma única plataforma</b>
+            <b> seus projetos em uma única plataforma</b>
           </Text>
         </Grid>
         <Grid
           onSubmit={handleSubmit(handleSignIn)}
           as="form"
-          mt="4"
-          w="50%"
+          w={["100%", "100%", "40%", "40%"]}
           padding="30px 15px"
           border="3px solid"
           borderColor="gray.100"
           bg="white"
           color="gray.900"
+          mt={["4", "4", "0", "0"]}
         >
           <Heading size="lg">Bem vindo de volta!</Heading>
           <VStack spacing="5" mt="6">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
 import { LogIn } from "../pages/Login";
+import { Route } from "./Route";
 
 export const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={LogIn}></Route>
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
